4c4e25bf01777b549ce61048b44c291c7cb2809c
Alexander Mendez <bluespace@gmail.com>
2014-06-10 00:04:47 -0700

    added new get script, and working on sleep time

---
+++ get.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ~/.limited/env
+
+APPFILE="f2c74a8ab2a40bdae1796ae48dc47e19a69a52b0"
+FILENAME="${HOME}/Library/Application Support/MobileSync/Backup/${UUID}/${APPFILE}"
+
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+OUTFILE="${SCRIPTPATH}/data/sleep_$(date +%Y.%m.%d).sqlite"
+CURRFILE="${SCRIPTPATH}/data/current.sqlite"
+
+
+if [ ! -f $OUTFILE ]; then
+    echo 'Updating data'
+    cp "$FILENAME" "$OUTFILE"
+    ln -sf "$(basename $OUTFILE)" "$CURRFILE"
+else
+    echo 'File already exists: $OUTFILE'
+fi 
--- parse.py
+++ parse.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 
 import sqlite3
+import dataset
 
 
-class Sleep(object):
-    def __init__(filename):
-        self.filename = filename
-        self.con = sqlite3.connect(filename)
-        
+# class Sleep(object):
+#     def __init__(filename):
+#         self.filename = filename
+#         self.con = sqlite3.connect(filename)
+#         
+# 
+# 
+# def parse(filename):
+#     sleepdata = Sleep(filename)
+#     sleepdata.parse()
 
+def debug():
+    db = dataset.connect('sqlite:///data/current.sqlite')
+    for item in db['ZLOG']:
+        print item
 
-def parse(filename):
-    sleepdata = Sleep(filename)
-    sleepdata.parse()
 
 
 if __name__ == '__main__':
-    filename = 'data/f2c74a8ab2a40bdae1796ae48dc47e19a69a52b0'
-    parse(filename)
+    # filename = 'data/f2c74a8ab2a40bdae1796ae48dc47e19a69a52b0'
+    # parse(filename)
+    
+    from pysurvey import util
+    util.setup_stop()
+    
+    debug()
